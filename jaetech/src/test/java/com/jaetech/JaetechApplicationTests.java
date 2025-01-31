d405e04027f9a6a3fa790537f1433c849c038ee1
백재우 <9712jw@gmail.com>
2025-02-01 05:38:23 +0900

    project init

---
+++ jaetech/src/test/java/com/jaetech/JaetechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaetechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

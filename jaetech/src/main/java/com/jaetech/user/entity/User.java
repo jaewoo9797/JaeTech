9e732d65dbf0f89e3b6c6550bbaf666e277e1e90
백재우 <9712jw@gmail.com>
2025-02-02 17:34:20 +0900

    feat: 엔티티 속성을 자바 객체로 매핑합니다.

---
+++ jaetech/src/main/java/com/jaetech/user/entity/User.java
@@ -0,0 +1,30 @@
+package com.jaetech.user.entity;
+
+import com.jaetech.user.entity.enums.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String nick;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+}
---
+++ jaetech/src/main/java/com/jaetech/user/entity/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.jaetech.user.entity.enums;
+
+public enum UserRole {
+    ADMIN, GUEST
+    ;
+}

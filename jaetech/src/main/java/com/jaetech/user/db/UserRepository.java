c6050fddb2c6acdcce6f3c41b7e0a1f1c8ad4c2f
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-02-03 13:42:53 +0900

    feat: User 양방향 연관관계 설정

---
+++ jaetech/src/main/java/com/jaetech/user/db/UserRepository.java
@@ -0,0 +1,7 @@
+package com.jaetech.user.db;
+
+import com.jaetech.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
--- jaetech/src/main/java/com/jaetech/user/entity/User.java
+++ jaetech/src/main/java/com/jaetech/user/entity/User.java
@@ -1,5 +1,7 @@
 package com.jaetech.user.entity;
 
+import com.jaetech.article.entity.Article;
+import com.jaetech.comment.entity.Comment;
 import com.jaetech.common.BaseEntity;
 import com.jaetech.user.entity.enums.UserRole;
 import jakarta.persistence.Column;
@@ -9,10 +11,20 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class User extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,4 +41,15 @@ public class User extends BaseEntity {
     @Column(nullable = false, length = 45)
     private UserRole role;
 
+    @OneToMany(mappedBy = "user")
+    private List<Article> articles = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Comment> comments = new ArrayList<>();
+
+    public User(String nick, String email, UserRole role) {
+        this.nick = nick;
+        this.email = email;
+        this.role = role;
+    }
 }
